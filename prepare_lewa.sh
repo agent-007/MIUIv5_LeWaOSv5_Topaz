17bef98b4a798b44bf2ce98207dd753032a3a687
Alex Gorbachenko <alex.gorbachenko@gmail.com>
2013-10-19 12:59:07 +0400

    move google play to the right place

--- prepare_lewa.sh
+++ prepare_lewa.sh
@@ -3,5 +3,5 @@ patch -p0 < LeWa-updater-script-s920-to-Topaz.patch
 rm -vf ./system/app/LewaAppStore.*
 rm -vf ./system/app/LewaGameCenter.*
 rm -vf ./system/vendor/operator/app/*
-cp GooglePlay.apk ./system/app/
+cp GooglePlay.apk ./system/vendor/operator/app/
 
