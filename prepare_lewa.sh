60a9754e4f7bdea8f1361f49281c8306a9316805
Alex Gorbachenko alex.gorbachenko@gmail.com <alex.gorbachenko@gmail.com>
2013-10-22 13:32:59 +0400

    fixes for LeWa

--- prepare_lewa.sh
+++ prepare_lewa.sh
@@ -3,7 +3,7 @@ patch -p0 < LeWa-updater-script-s920-to-Topaz.patch
 rm -vf ./system/app/LewaAppStore.*
 rm -vf ./system/app/LewaGameCenter.*
 rm -vf ./system/vendor/operator/app/*
-cp -vf libcameracustom.so /system/lib/
-cp -vf camera.default.so /system/lib/hw/
+cp -vf libcameracustom.so ./system/lib/
+cp -vf camera.default.so ./system/lib/hw/
 cp -vf GooglePlay.apk ./system/vendor/operator/app/
 
